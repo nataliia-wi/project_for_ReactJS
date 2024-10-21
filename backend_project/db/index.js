758dcacc59fd3e6132ad343f011b476a6b91b542
Nataliia S <natalilu1990@gmail.com>
2024-10-21 19:06:17 +0300

    add backend files

---
+++ backend_project/db/index.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("reactjs", "root", "root", {
+    dialect: "mysql",
+    host: "localhost",
+    port: 3306
+});
+
+const Notes = require('./notes')(sequelize);
+
+module.exports = {
+    sequelize: sequelize,
+    notes: Notes
+}
